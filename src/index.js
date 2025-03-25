bfa87dc0dafc8d17ad457a6ee90308df26819770
KinguBaka <tarikab92@yahoo.fr>
2025-03-25 13:21:46 +0100

    Change to Guild Message

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.MessageReactions,
+    IntentsBitField.Flags.GuildMessageReactions,
   ],
 });
 
