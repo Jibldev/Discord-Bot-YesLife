b09f9c9ac4b7ba71783762b25b19794d37fd1b28
Antoine Mistral <antoinemistral.dev@gmail.com>
2025-03-26 14:59:29 +0100

    Ajout de !debugcron

--- src/index.js
+++ src/index.js
@@ -144,6 +144,34 @@ client.on("messageReactionAdd", (reaction, user) => {
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
 
+  if (message.content === "!debugcron") {
+    const now = new Date().toLocaleString("fr-FR", {
+      timeZone: "Europe/Paris",
+    });
+    const [date, time] = now.split(", ");
+    const [currentHour, currentMinute] = time.split(":");
+    const currentTime = `${currentHour}:${currentMinute}`;
+
+    let settingHour = "Inconnu";
+    let settingMessage = "Aucun message programmé.";
+    const guildId = message.guild.id;
+
+    if (fs.existsSync("settings.json")) {
+      const settings = JSON.parse(fs.readFileSync("settings.json", "utf8"));
+      const setting = settings[guildId];
+      if (setting) {
+        settingHour = setting.hour;
+        settingMessage = setting.message;
+      }
+    }
+
+    message.reply(
+      `🕒 **Heure actuelle perçue par le bot** : \`${currentTime}\`\n` +
+        `📅 **Heure programmée** : \`${settingHour}\`\n` +
+        `💬 **Message programmé** : ${settingMessage}`
+    );
+  }
+
   const content = message.content.toLowerCase();
 
   if (content.startsWith("!setchannel")) {
