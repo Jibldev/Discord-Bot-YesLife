115f27bfb8c3e8a43d4ad6228d5561489943e945
Antoine Mistral <antoinemistral.dev@gmail.com>
2025-03-26 12:32:02 +0100

    Test 3

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const client = new Client({
 });
 
 cron.schedule(
-  "33 12 * * *",
+  "37 12 * * *",
   async () => {
     if (!fs.existsSync("channels.json")) return;
     const channels = JSON.parse(fs.readFileSync("channels.json", "utf8"));
@@ -47,7 +47,7 @@ cron.schedule(
         const channel = await client.channels.fetch(channels[guildId]);
         if (channel) {
           channel.send("Bonjour ! Voici ton message quotidien à 15h22 ! 🚀");
-          await sentMessage.react("✅"); // Ajoute automatiquement la réaction
+          sentMessage.react("✅"); // Ajoute automatiquement la réaction
         } else {
           console.error(`Canal introuvable pour le serveur ${guildId}`);
         }
